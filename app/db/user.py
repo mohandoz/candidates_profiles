4672972084145e9498ea86c6651ecb7434e38141
mohandoz3 <mohannad.ahmad@mawdoo3.com>
2022-11-26 21:48:23 +0300

    Add UserManager

--- app/db/user.py
+++ app/db/user.py
@@ -4,7 +4,12 @@ from uuid import UUID, uuid4
 from beanie import PydanticObjectId
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager
-from fastapi_users.db import BaseOAuthAccount, BeanieBaseUser, BeanieUserDatabase
+from fastapi_users.db import (
+    BaseOAuthAccount,
+    BeanieBaseUser,
+    BeanieUserDatabase,
+    ObjectIDIDMixin,
+)
 from pydantic import Field
 from pymongo import IndexModel
 
@@ -25,3 +30,22 @@ class UserDoc(BeanieBaseUser[PydanticObjectId]):
 
 async def get_user_db():
     yield BeanieUserDatabase(UserDoc)
+
+
+class UserManager(ObjectIDIDMixin, BaseUserManager[UserDoc, PydanticObjectId]):
+    async def on_after_register(self, user: UserDoc, request: Optional[Request] = None):
+        print(f"User {user.id} has registered.")
+
+    async def on_after_forgot_password(
+        self, user: UserDoc, token: str, request: Optional[Request] = None
+    ):
+        print(f"User {user.id} has forgot their password. Reset token: {token}")
+
+    async def on_after_request_verify(
+        self, user: UserDoc, token: str, request: Optional[Request] = None
+    ):
+        print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+
+async def get_user_manager(user_db: BeanieUserDatabase = Depends(get_user_db)):
+    yield UserManager(user_db)
